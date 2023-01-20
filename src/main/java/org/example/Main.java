f25bdd54ccff4b1db7f980a446ddb3d8b404ba0d
timur_ziya <timka_ziya@mail.ru>
2023-01-20 15:53:45 +0300

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\ADmin\\Desktop\\test1.txt";
+        String path = "C:\\Users\\Тимур\\Desktop\\test1.txt";
         File file = new File(path);
         String input;
         String[] arr = new String[1000];
@@ -18,10 +18,9 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         while ((input = bufferedReader.readLine()) != null) {
             if (!input.equals("")) {
-                char[] inputChar = input.toCharArray();
-                inputChar[2] = ' ';
-                inputChar[5] = '.';
-                input = String.valueOf(inputChar);
+                input = input.substring(0,7);
+                input = new StringBuilder(input).insert(2,"°").insert(5, ".").toString();
+//                input = input + "'";
                 arr[i] = input.replace("B", "8")
                         .replace("C","0")
                         .replace("E", "8")
@@ -37,16 +36,17 @@ public class Main {
         String[] arrE = new String[(i) / 2];
         System.arraycopy(arr, i / 2, arrE, 0, (i) / 2);
 
-//             Joining two coordinates into ine line
-        for (i = 0; i < arrE.length; i++) {
-            arrN[i] = arrN[i] + " " + arrE[i];
-        }
+//             Joining two coordinates into one line
+//        for (i = 0; i < arrE.length; i++) {
+//            arrN[i] = arrN[i] + " " + arrE[i];
+//        }
 //        Arrays.stream(arrN).forEach(System.out::println);
         Main main = new Main();
-        main.writeDataToExcel(arrN, "19Layer");
+        main.writeDataToExcel(arrN, arrE, "20");
+        System.out.println(i%2==0);
     }
 
-    public void writeDataToExcel(String[] arr1, String fileName) {
+    public void writeDataToExcel(String[] arr1, String [] arr2, String fileName) {
 
         // workbook object
         XSSFWorkbook workbook = new XSSFWorkbook();
@@ -64,12 +64,16 @@ public class Main {
         // writing the data into the sheets...
 
         row = spreadsheet.createRow(0);
-        Cell cell = row.createCell(0);
-        cell.setCellValue("Coordinates");
+        Cell cell = row.createCell(1);
+        cell.setCellValue("longitude");
+        Cell cell0 = row.createCell(0);
+        cell0.setCellValue("latitude");
         for (int i = 0; i < arr1.length; i++) {
             row = spreadsheet.createRow(i + 1);
             Cell cell1 = row.createCell(0);
             cell1.setCellValue(arr1[i]);
+            Cell cell2 = row.createCell(1);
+            cell2.setCellValue(arr2[i]);
 
         }
 
@@ -78,10 +82,10 @@ public class Main {
         // writing the workbook into the file...
         try (
                 FileOutputStream out = new FileOutputStream(
-                        new File("C:\\Users\\ADmin\\Desktop\\new excel\\" + fileName + ".xlsx"))) {
+                        new File("C:\\Users\\Тимур\\Desktop\\new excel\\" + fileName + ".xlsx"))) {
 
             workbook.write(out);
-            System.out.printf("File '%s' has been created!", fileName);
+            System.out.printf("File '%s' has been created! \n", fileName);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
